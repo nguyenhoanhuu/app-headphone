9f8e267ff9316f4e34017451ec232be50a1b5e10
Nguyen Hoan Huu <nguyenhoanhuu8668@gmail.com>
2022-11-14 22:15:07 +0700

    create database & finish UI home

---
+++ components/screens/MyCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const MyCart = () => {
+    return (
+        <View>
+            <Text>UI</Text>
+        </View>
+    );
+};
+
+export default MyCart;
---
+++ components/screens/ProductInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ProductInfo = () => {
+    return (
+        <View>
+            <Text>UI</Text>
+        </View>
+    );
+};
+
+export default ProductInfo;
