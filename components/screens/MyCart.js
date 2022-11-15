2022bb8c8fb075b8aa288033ff18f78340374376
ui2 <phanhongha96@gmail.com>
2022-11-15 18:54:42 +0700

    init screen MyCart, header screen.

--- components/screens/MyCart.js
+++ components/screens/MyCart.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-const MyCart = () => {
-    return (
-        <View>
-            <Text>UI</Text>
+const MyCart = ({ navigation }) => {
+  const [product, setProduct] = useState();
+  const [total, setTotal] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getDataFromDB();
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: COLOURS.white,
+        position: "relative",
+      }}
+    >
+      <ScrollView>
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            paddingTop: 16,
+            paddingHorizontal: 16,
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <MaterialCommunityIcons
+              name="chevron-left"
+              style={{
+                fontSize: 18,
+                color: COLOURS.backgroundDark,
+                padding: 12,
+                backgroundColor: COLOURS.backgroundLight,
+                borderRadius: 12,
+              }}
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 14,
+              color: COLOURS.black,
+              fontWeight: "400",
+            }}
+          >
+            Order Details
+          </Text>
+          <View></View>
         </View>
-    );
+        
+       
+      </ScrollView>
+    </View>
+  );
 };
 
 export default MyCart;
